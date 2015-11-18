27bd60d404978940f754b56bec65c507b78160e8
ewagner46 <ewagner46@gmail.com>
2015-11-18 16:09:09 -0500

    Some tests by Emily and Jose

---
+++ crowd_childb4parent.c
@@ -0,0 +1,28 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define TEST_NAME "318_childb4parent"
+#include "318_test-tapish.h"
+
+/*Authors: Emily and Jose*/
+
+/*Test exiting the child before the parent.  Both should still get cleaned
+up -- the parent cleaned by sh, the child by initproc because the parent 
+doesn't wait and the child is thus abandoned.
+* Shakes out sleeping channel bugs.  Recommended to ^P after to see process
+and thread statuses.*/
+
+int main() {
+ TEST_STRT(2);
+ int f = fork();
+ if (f < 0) {
+   TEST_EXIT("Fork failed");
+ } else if (f == 0) {
+   TEST_FINI("child"); /* success 1 */
+ } else {
+   sleep(20);
+   TEST_FINI("parent"); /* success 2 */
+ }
+ exit();
+}
